39b18648d7f972d5eb79cb28706959349ed0bc9b
tbluche <>
2017-09-22 13:00:08 +0200

    ctd

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='myskill',
+    name='spotifycontroller',
     version='0.0.1',
     description='My skill for Snips',
-    url='https://github.com/snipsco/snips-skill-skeleton',
+    url='https://github.com/tbluche/spotify-controller',
     download_url='',
     license='MIT',
-    install_requires=['requests==2.6.0'],
+    install_requires=['requests==2.6.0', 'spotipy==2.4.4'],
     test_suite="tests",
     keywords=['snips'],
-    packages=['myskill'],
-    package_data={'myskill': ['Snipsspec']},
+    packages=['spotifycontroller'],
+    package_data={'spotifycontroller': ['Snipsspec']},
     include_package_data=True,
 )
--- spotifycontroller/spotifycontroller.py
+++ spotifycontroller/spotifycontroller.py
@@ -1,25 +1,21 @@
 # -*-: coding utf-8 -*-
 """ Skeleton Snips skill. """
 
-class MySkill:
+class SpotifyController:
     """ Skeleton Snips skill. """
 
-    def __init__(self, hostname, light_ids):
+    def __init__(self):
         """
         :param hostname: hostname for some IoT device
         :param light_ids: A list of light IDs
         """
-        self.hostname = hostname
-        self.light_ids = light_ids
+        # self.hostname = hostname
+        # self.light_ids = light_ids
 
-    def turn_on(self):
+    def stop_playing(self):
         """ Turn on something. """
-        print("Turn on")
-        
-    def turn_off(self):
-        """ Turn of something. """
-        print("Turn off")
+        print("Stop the player")
 
-    def set_color_name(self, object_color):
-        """ Set an object color. """
-        print("Set object color to {}".format(object_color))
+    def resume_playing(self):
+        """ Turn of something. """
+        print("Resume playing")
