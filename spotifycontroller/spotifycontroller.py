63fbe50e895311219ad87145d72157390eb5c882
tbluche <>
2017-09-22 14:49:03 +0200

    connect with spotify

--- spotifycontroller/spotifycontroller.py
+++ spotifycontroller/spotifycontroller.py
@@ -1,21 +1,59 @@
 # -*-: coding utf-8 -*-
 """ Skeleton Snips skill. """
+import os
+import sys
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 class SpotifyController:
     """ Skeleton Snips skill. """
 
-    def __init__(self):
+    def __init__(self, client_id, secret_id, redirect_url, tts_service=None):
         """
-        :param hostname: hostname for some IoT device
-        :param light_ids: A list of light IDs
         """
+        self.client_id = client_id
+        self.secret_id = secret_id
+        self.redirect_url = redirect_url
+        self.tts = tts_service
         # self.hostname = hostname
         # self.light_ids = light_ids
 
+    def __get_access_token(self):
+        scope = 'user-modify-playback-state user-read-playback-state'
+        auth = SpotifyOAuth(
+            self.client_id, self.secret_id, self.redirect_url,
+            scope=scope, cache_path='.cache'
+        )
+        token = auth.get_cached_token()
+        if token is None:
+            auth_url = auth.get_authorize_url()
+            try:
+                import webbrowser
+                webbrowser.open(auth_url)
+                print("Opened %s in your browser" % auth_url)
+            except:
+                self.tts.speak("Please navigate here: %s" % auth_url)
+                print("Please navigate here: %s" % auth_url)
+                print
+
+            response = raw_input("Enter the URL you were redirected to: ")
+            code = auth.parse_response_code(response)
+            token = auth.get_access_token(code)
+        else:
+            if auth.is_token_expired(token):
+                token = auth.refresh_access_token(token['refresh_token'])
+
+        return token['access_token']
+
+
     def stop_playing(self):
         """ Turn on something. """
+        sp = spotipy.Spotify(auth=self.__get_access_token())
         print("Stop the player")
+        sp.pause_playback()
 
     def resume_playing(self):
         """ Turn of something. """
+        sp = spotipy.Spotify(auth=self.__get_access_token())
         print("Resume playing")
+        sp.start_playback()
