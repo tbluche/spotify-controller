1617a03d9e280c28051320cb4024a70974c7d728
tbluche <>
2017-09-22 16:35:41 +0200

    track info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='spotifycontroller',
-    version='0.0.6',
+    version='0.0.7',
     description='My skill for Snips',
     url='https://github.com/tbluche/spotify-controller',
     download_url='',
